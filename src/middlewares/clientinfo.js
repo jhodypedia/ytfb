523bd106327af12aaec6999598a08ef6c99fc54a
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-27 09:48:11 +0700

    Create clientinfo.js

---
+++ src/middlewares/clientinfo.js
@@ -0,0 +1,6 @@
+import UAParser from "ua-parser-js";
+export const detectClient = (req,res,next)=>{
+  const parser = new UAParser(req.headers["user-agent"]);
+  req.clientInfo = { device: parser.getResult(), timezone: req.headers["x-timezone"] || "UTC" };
+  next();
+};
